1968109a58df448080a7a9e271eb2501793232bf
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-03-13 12:29:10 +0000

    Organizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Manage your building with us.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
