7f4d646e9103b9bbe9bc3669bfb96dc2051e6c28
Ric Az <ricardoazevedo96@gmail.com>
2020-03-13 14:34:42 +0000

    to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import 'assets/plugins/nucleo/css/nucleo.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'assets/scss/argon-dashboard-react.scss';
 import AdminLayout from 'layouts/Admin.js';
 import AuthLayout from 'layouts/Auth.js';
 import './App.css';
