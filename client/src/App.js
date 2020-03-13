382cac1de20e1c68ff49af84662c37995c426eca
Ric Az <ricardoazevedo96@gmail.com>
2020-03-13 12:27:31 +0000

    use app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import AdminLayout from 'layouts/Admin.js';
+import AuthLayout from 'layouts/Auth.js';
+import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
+    </Switch>
     </div>
   );
 }
