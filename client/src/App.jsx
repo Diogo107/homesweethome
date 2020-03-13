412ebd34931289e87e7e1cbb74945fab8480e91f
Luca Campora <luca.campora@gmail.com>
2020-03-13 17:46:16 +0000

    pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,21 @@ import 'assets/plugins/nucleo/css/nucleo.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'assets/scss/argon-dashboard-react.scss';
 import './App.css';
-import Home from './views/Home/home';
+
 
 import AdminLayout from 'layouts/Admin.js';
 import AuthLayout from 'layouts/Auth.js';
+import LandingPage from 'views/LandingPage'
 import './App.css';
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/" component={LandingPage} exact/>
         <Route path="/admin" render={props => <AdminLayout {...props} />} />
         <Route path="/auth" render={props => <AuthLayout {...props} />} />
-        <Redirect from="/" to="/admin/index" />
-        <Route path="/" exact component={Home} />
       </Switch>
     </div>
   );
